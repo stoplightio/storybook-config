a31721f71cf7261bc921ce7d30d4cec99f0a952a
Jakub Rożek <jakub@stoplight.io>
2019-01-13 00:39:44 +0100

    feat: upgrade to Storybook v5

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,3 @@
-// @ts-ignore
 import { withOptions } from '@storybook/addon-options';
 import { addDecorator, configure } from '@storybook/react';
 import * as React from 'react';
@@ -6,9 +5,7 @@ import { withThemes } from './custom-addons/withThemes';
 
 addDecorator(
   withOptions({
-    // @ts-ignore
     name: pkg.name || 'Stoplight UI-Kit',
-    // @ts-ignore
     url: pkg.url || 'https://github.com/stoplightio/ui-kit',
     goFullScreen: false,
     showStoriesPanel: true,
@@ -26,7 +23,7 @@ addDecorator(withThemes);
 
 addDecorator(storyFn => <div>{storyFn()}</div>);
 
-// configure(require('@project/stories'), module);
+configure(require('@project/stories'), module);
 
 function loadStories() {
   require('@project/stories');
