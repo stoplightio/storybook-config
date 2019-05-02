4cecab2cf7cbd49aa39de84de8575e88f521b429
Jakub Rożek <jakub@stoplight.io>
2019-05-02 15:40:52 +0200

    feat: process styles

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -11,9 +11,6 @@ module.exports = ({ config }: any) => {
   config.context = cwd;
   config.mode = 'development';
   config.resolve.alias['@project/stories'] = require.resolve('src/__stories__/index.ts', { paths: [cwd] });
-  config.resolve.alias['@project/theme'] = require.resolve('.storybook/theme', {
-    paths: [cwd],
-  });
 
   config.plugins.push(
     new webpack.DefinePlugin({
@@ -23,19 +20,89 @@ module.exports = ({ config }: any) => {
   );
 
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
+    test: /\.tsx?$/,
     include: [path.resolve(cwd, 'src')],
     use: [
       {
         loader: require.resolve('ts-loader'),
         options: {
+          transpileOnly: true,
           onlyCompileBundledFiles: true, // https://github.com/TypeStrong/ts-loader#onlycompilebundledfiles-boolean-defaultfalse
         },
       },
     ],
   });
 
-  config.resolve.extensions.push('.ts', '.tsx');
+  config.module.rules.push({
+    test: /\.css$/,
+    include: [path.resolve(cwd, 'src')],
+    use: [
+      {
+        loader: require.resolve('style-loader'),
+        options: {
+          sourceMap: true,
+        },
+      },
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          sourceMap: true,
+          importLoaders: 1,
+        },
+      },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          sourceMap: true,
+          plugins: [
+            require('autoprefixer')({
+              env: 'last 2 Chrome versions, last 2 Firefox versions, last 1 Safari version',
+            })
+          ]
+        },
+      },
+    ],
+  });
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    include: [path.resolve(cwd, 'src')],
+    use: [
+      {
+        loader: require.resolve('style-loader'),
+        options: {
+          sourceMap: true,
+        },
+      },
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          sourceMap: true,
+          importLoaders: 2,
+        },
+      },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          sourceMap: true,
+          plugins: [
+            require('autoprefixer')({
+              env: 'last 2 Chrome versions, last 2 Firefox versions, last 1 Safari version',
+            })
+          ]
+        },
+      },
+      {
+        loader: require.resolve('sass-loader'),
+        options: {
+          sourceMap: true,
+          includePaths: [path.resolve(cwd, 'node_modules')],
+        },
+      },
+    ],
+  });
+
+  config.resolve.extensions.push('.ts', '.tsx', '.jsx', '.js');
 
   return config;
 };
